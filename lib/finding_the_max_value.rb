eb230089e1bf4ef09adb9b37ae1339fbc1ce6cac
erin dougher <ejdougher@gmail.com>
2020-11-25 04:38:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,14 @@
 def find_max_value(array)
-  # Add your solution here
+  n = 0
+  array = hill
+  max_element = 0
+  while n < array.length
+    if max_element < array[n] do
+      max_element = array[n]
+    end
+    n += 1
+    p max_element
 end
+
+hill = [1,2,3,4,5,4,3,2,1]
+find_max_value(hill)
